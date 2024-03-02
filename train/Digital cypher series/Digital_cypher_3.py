ff778142df3e6857d4b5c63154f45b241467fb72
hasan foraty <hasanforaty19@gmail.com>
2024-03-02 16:25:44 +0330

    Digital_cypher_3 init

---
+++ train/Digital cypher series/Digital_cypher_3.py
@@ -0,0 +1,7 @@
+def find_the_key(message, code):
+    pass
+
+
+test1 = find_the_key("scout", [20, 12, 18, 30, 21]) == 1939
+test2 = find_the_key("masterpiece", [14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8]) == 1939
+test3 = find_the_key("nomoretears", [15, 17, 14, 17, 19, 7, 21, 7, 2, 20, 20]) == 12
